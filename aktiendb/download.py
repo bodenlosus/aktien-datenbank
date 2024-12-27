5fcf1d518991701bbb126bdd4ff25fabd722f018
bodenlosus <johannes.till.schmidt@outlook.de>
2024-12-27 21:29:44 +0100

    a

--- aktiendb/download.py
+++ aktiendb/download.py
@@ -10,12 +10,10 @@ def getID(stocks, symbol:str ) -> int:
         raise ValueError(f"No stock found for {symbol}")
 
     return id
-def downloadStocks(stocks: dict[str, str], start:str=None, end:str=None, period="1y") -> DataFrame:
-    symbols = " ".join(stocks.keys())
-    tickers = yf.Tickers(symbols)
-    dataframe = (tickers.history(period=period, start=start, end=end)
+def downloadStocks(symbol, id, start:str=None, end:str=None, period="1y") -> DataFrame:
+    ticker = yf.Ticker(symbol)
+    dataframe = (ticker.history(period=period, start=start, end=end)
                  .dropna()
-                 .stack(level=1, future_stack=True)
                  .reset_index()
                  )
     
@@ -28,9 +26,9 @@ def downloadStocks(stocks: dict[str, str], start:str=None, end:str=None, period=
     columnsToDrop = [column for column in dataframe.columns if column not in columnsToKeep] 
     
     dataframe.drop(columns=columnsToDrop, errors="ignore", inplace=True)
-    dataframe.columns = ["timestamp", "symbol", "close", "high", "low", "open", "volume"]
+    dataframe.columns = ["timestamp", "close", "high", "low", "open", "volume"]
     
-    dataframe["stock_id"] = dataframe["symbol"].apply(lambda symbol: getID(stocks, symbol))
+    dataframe["stock_id"] = id
     dataframe["timestamp"] = dataframe["timestamp"].apply(lambda dt: dt.strftime("%Y-%m-%d"))
     dataframe = dataframe.astype({"volume":"int"})
     return dataframe
