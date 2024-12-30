79571858026c99c5d39712ec8a34e281f43f27c5
johannes <johannes@fedora.fritz.box>
2024-12-30 01:20:19 +0100

    to python39 :(

---
+++ build/lib/aktiendb/updateDatabase.py
@@ -0,0 +1,67 @@
+import sys
+import time
+from .download import downloadStocks
+from dotenv import dotenv_values
+from supabase import Client
+import yfinance as yf
+import pathlib
+from .database.insert_prices import uploader
+from .database.stock_info import getStockInfo
+from .database.client import createSupabaseClient
+from .track_records import TrackRecord
+import queue
+import threading
+
+
+def getStocks(stockInfos) -> dict[str, int]:
+    stocks = {symbol.upper():id for id, symbol in stockInfos}
+    return stocks
+
+def update():
+    url, key = getSecrets()
+    
+    supabase: Client = createSupabaseClient(timeout=30, url=url, key=key)
+    
+    stockInfos = getStockInfo(supabase, keys=("id", "symbol"))
+    
+    q = queue.Queue()
+    
+    worker = threading.Thread(target=uploader, args=(q, supabase, 1500))
+    worker.start()
+    
+    record_file = pathlib.Path("./record.dat")
+    record_file.touch(exist_ok=True)
+    
+    record = TrackRecord(record_file)
+    record.readRecord()
+    
+    for id, symbol in stockInfos:
+        print("Downloading stock data for", symbol, id, sep=" ")
+        
+        period = record.getUpdatePeriod(id)
+
+        dataframe = downloadStocks(symbol=symbol, id=id, period=period)
+        time.sleep(0.1)
+        if dataframe.empty:
+            continue
+        
+        q.put((dataframe, dataframe.shape[0]))
+    
+    q.put((None, None))
+    
+    worker.join()
+
+def getSecrets():
+    config = dotenv_values(".env")
+    url = config.get("SUPABASE_URL")
+    key = config.get("SUPABASE_SERVICE_KEY")
+    
+    if not url:
+        url = sys.argv[1]
+    if not key:
+        key = sys.argv[2]
+
+    return(url, key)
+
+if __name__ == "__main__":
+    update()
---
+++ lib/python3.9/site-packages/aktiendb/track_records.py
@@ -0,0 +1,85 @@
+from typing import Union
+import numpy as np
+import time
+
+
+class TrackRecord:
+    def __init__(self, file_path):
+        self.file_path = file_path
+        self.record = dict()
+
+    def updateRecord(self, stock_ids: list[int]):
+        currentTime: str = time.strftime("%Y-%m-%d_%H:%M:%S", time.gmtime())
+
+        for id in stock_ids:
+            self.record[id] = currentTime
+
+        self.saveRecord()
+
+    def saveRecord(self):
+        with open(self.file_path, "w") as file:
+            file.writelines([f"{id},{time}\n" for id, time in self.record.items()])
+
+    def readRecord(self) -> dict[int, Union[str, None]]:
+        with open(self.file_path, "r") as file:
+            recordLines = [line.strip().split(",") for line in file.readlines()]
+
+        for id, timestamp in recordLines:
+            self.record[int(id)] = timestamp
+        
+        return self.record
+
+    def getDaysSinceLastUpdate(self, timestamp) -> int:
+            currentTime = time.mktime(time.gmtime())
+            updateTime = time.mktime(time.strptime(timestamp, "%Y-%m-%d_%H:%M:%S"))
+
+            timeSinceUpdate = (currentTime - updateTime) / (60 * 60 * 24)
+
+            daysSinceUpdate = int(np.ceil(timeSinceUpdate))
+
+            return daysSinceUpdate
+    
+    def getLastUpdateTimestamp(self, id) -> str:
+        timestamp = self.record[id]
+    
+    def getMaxUpdateData(self, ids: list[int]) -> str:
+        maxID = max(ids, key=lambda id: self.getDaysSinceLastUpdate(self.record[id]))
+        timestamp = self.getLastUpdateTimestamp(maxID)
+        period = self.getUpdatePeriod(maxID)
+        
+        return timestamp, period
+    
+    def getCurrentDate(self):
+        currentTime = time.strftime("%Y-%m-%d", time.gmtime())
+        return currentTime
+
+    def getUpdatePeriod(self, id) -> str:
+        possibleIntervals = {
+            1: "1d",
+            5: "5d",
+            31: "1mo",
+            31 * 3: "3mo",
+            31 * 6: "6mo",
+            366: "1y",
+            366 * 2: "2y",
+            366 * 5: "5y",
+            366 * 10: "10y",
+        }
+        if id not in self.record.keys():
+            return "2y"
+        
+        timestamp = self.record[id]
+        
+        days = self.getDaysSinceLastUpdate(timestamp)
+        
+        for pDays, period in possibleIntervals.items():
+            if pDays >= days:
+                return period
+
+        return "max"
+
+if __name__ == "__main__":
+    record = TrackRecord("./record.dat")
+    print(record.readRecord())
+    record.updateRecord((2, 3, 4))
+    print(record.getUpdatePeriod(1))
